d616dbaca228b4c20cca4e83d016c591e92b28c6
unknown <=>
2020-11-23 20:41:23 -0500

    Jquery_23_11_2020

---
+++ main.js
@@ -0,0 +1,26 @@
+let nombre="Nelson Enrique";
+let apellido="Murillo Robles";
+let edad="35";
+let genero="Mssculino";
+let camiseta="Tengo una camiseta roja";
+let color_de_pelo="negro";
+let color_de_ojos="cafe";
+let email="nelsonmurillopedro@gmail.com"
+let deporte_favorito="Futbol";
+let anime_favorito="naruto";
+let altura="1.80";
+let peso="80";
+let hobbie_favorito="Programar";
+
+console.log("Mi nombre es:"+nombre);
+console.log("Mi apellido es:"+apellido);
+console.log("Mi edad es:"+edad);
+console.log("Mi correo es:"+email);
+console.log("Mi genero es:"+genero);
+console.log("Mi color de pelo es"+color_de_pelo);
+console.log("Mi color de ojos es:"+color_de_ojos);
+console.log("Mi deporte favorito es:"+deporte_favorito);
+console.log("Mi anime favorito es:"+anime_favorito);
+console.log("Mi estatura es:"+altura+" Metros");
+console.log("Mi peso es:"+peso+" Kilos");
+console.log("Mi hobbie favorito es:"+hobbie_favorito);
